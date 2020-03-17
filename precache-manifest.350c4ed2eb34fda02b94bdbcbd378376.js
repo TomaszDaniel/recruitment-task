c7c19919d9abf7be5bffca97b09d5c4f88da1741
Tomasz Daniel <tomaszz.daniel@gmail.com>
2020-03-17 23:59:16 +0100

    Updates

---
+++ precache-manifest.350c4ed2eb34fda02b94bdbcbd378376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bb1d3d0ded36ab407d75975d668d83",
+    "url": "/recruitment-task/index.html"
+  },
+  {
+    "revision": "005141821c6818825542",
+    "url": "/recruitment-task/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "7ae22a1826266473aead",
+    "url": "/recruitment-task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "005141821c6818825542",
+    "url": "/recruitment-task/static/js/2.30215cc2.chunk.js"
+  },
+  {
+    "revision": "ccbb9836384252930e3bbeb9a02b5166",
+    "url": "/recruitment-task/static/js/2.30215cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae22a1826266473aead",
+    "url": "/recruitment-task/static/js/main.f9e35828.chunk.js"
+  },
+  {
+    "revision": "7a643b8e8e3d9595fa62",
+    "url": "/recruitment-task/static/js/runtime-main.19666cdf.js"
+  }
+]);
